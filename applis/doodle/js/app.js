01184a14a8aeb26a49a9b28fb23bda47e3aa52a3
brianBasco <bast620@gmail.com>
2019-02-27 17:53:10 +0100

    doodle app functions

--- applis/doodle/js/app.js
+++ applis/doodle/js/app.js
@@ -1,3 +1,9 @@
+//namespace doodleData
+doodleData = {
+    dates:["19 février 1983"],
+    presences: ["seb", "ju"]
+}
+
 document.addEventListener("DOMContentLoaded", function (event) {
 
     /*
@@ -27,6 +33,10 @@ document.addEventListener("DOMContentLoaded", function (event) {
    document.getElementById("fermerPageAjout").addEventListener("click", function(event) {
        fermerPageAjout();
    })
+
+   ajouterData();
+   btnInscription(); 
+   creerUneDate(doodleData.dates);
 })
 
 function ouvrirPageAjout() {
@@ -39,4 +49,57 @@ function fermerPageAjout() {
 
     let div = $("#pageAjout");
     div.removeClass("ouverte");
+}
+
+function ajouterData() {
+
+    let affichage = "il y a des doodles";
+    let dates = doodleData.dates;
+
+    if(dates.length == 0) affichage = "pas de doodle";
+    else affichage = dates[0];
+
+    let p = document.createElement("p");
+    p.innerHTML = affichage;
+
+    document.getElementById("app").appendChild(p);
+}
+
+function creerUneDate(date) {
+
+    //container à attacher à la div #app
+
+    let container = document.createElement("div");
+    container.className = "container uneDate";
+
+    let inputDate = document.createElement("input");
+    inputDate.setAttribute("readonly", "readonly");
+    inputDate.setAttribute("type", "text");
+    inputDate.value = date;
+
+    let inputNbreJoueurs = document.createElement("input");
+    inputNbreJoueurs.setAttribute("readonly", "readonly");
+    inputNbreJoueurs.setAttribute("type", "text");
+    inputNbreJoueurs.value = "nbre de participants";
+
+    //test
+    container.appendChild(inputDate);
+    container.appendChild(inputNbreJoueurs);
+
+    document.getElementById("app").appendChild(container);
+
+}
+
+function btnInscription() {
+
+    let container = document.createElement("div");
+    container.className = "container btnInscription";
+
+    let btnInscrire = document.createElement("button");
+    btnInscrire.className = "btn";
+    btnInscrire.innerHTML = "s'inscrire !";
+
+    container.appendChild(btnInscrire);
+
+    document.getElementById("app").appendChild(container);
 }
